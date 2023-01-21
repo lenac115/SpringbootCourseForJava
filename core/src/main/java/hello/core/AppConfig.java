1a48556ddb2b0206af0a570e26f49df54463e2dc
lenac115 <55846290+lenac115@users.noreply.github.com>
2023-01-22 00:28:49 +0900

    임시저장
    옵션관리 부분 강의 임시 저장 코드입니다.

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -48,7 +48,7 @@ public class AppConfig {
     @Bean
     public OrderService orderService() {
         System.out.println("call AppConfig.orderService");
-        return new OrderSerivceImpl(memberRepository(), new RateDiscountPolicy());
+        return new OrderSerivceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
